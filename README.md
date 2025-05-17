ba10512a59ba9a3ac4890573c26a92259c22065a
Facundo Muruchi <facundomuruchi01@gmail.com>
2025-05-17 14:41:14 -0300

    while true

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ This project implements a simple computer vision system to detect people in vide
 The script:
 
 - Loads a local video file (`subway.mp4`).
-- Processes 1 out of every 4 frames to optimize cost and analysis time.
+- Processes 1 out of every 5 frames to optimize cost and analysis time.
 - Sends each selected frame to AWS Rekognition for object detection.
 - Filters detections to only include the class "Person".
 - Saves detection annotations in `.txt` files (format: center_x, center_y, width, height).
@@ -17,13 +17,19 @@ The script:
 - Saves frames and annotations in an S3 Bucket.
 
 ---
+## Explanation
 
+https://www.notion.so/Computer-Vision-Person-Detection-with-AWS-1f48d5dc55c2803d8c03da34aef18448?pvs=4
+
+---
 ## Requirements
 
 - Python 3.x
 - Boto3
 - OpenCV (cv2)
 - Valid AWS credentials with Rekognition and S3 permissions
+
+---
 
 Install required libraries via pip:
 
--- main.py
+++ main.py
@@ -29,67 +29,67 @@ video = cv2.VideoCapture('subway.mp4')
 frame_nmr = -1
 
 # read frames
-flag = True
-while flag:
+while True:
     flag, frame = video.read()
 
-    if flag:
+    if not flag:
+        break
 
-        frame_nmr += 1
+    frame_nmr += 1
 
-        # process 1 out of 4 frames
-        if frame_nmr % 4 != 0:
-            continue
+    # process 1 out of 5 frames
+    if frame_nmr % 5 != 0:
+        continue
 
-        # save raw frames
-        raw_frame = frame.copy()
+    # save raw frames
+    raw_frame = frame.copy()
 
-        # preparing for aws rekognition
-        H, W, _ = frame.shape
-        # convert frame to jpg
-        _, buffer = cv2.imencode('.jpg', frame)
-        # convert buffer to bytes
-        frame_bytes = buffer.tobytes()
+    # preparing for aws rekognition
+    H, W, _ = frame.shape
+    # convert frame to jpg
+    _, buffer = cv2.imencode('.jpg', frame)
+    # convert buffer to bytes
+    frame_bytes = buffer.tobytes()
 
-        # detect objects
-        response = reko.detect_labels(Image={'Bytes' : frame_bytes},
-                                            MinConfidence = 50)
+    # detect objects
+    response = reko.detect_labels(Image={'Bytes' : frame_bytes},
+                                        MinConfidence = 50)
 
-        anns_buffer = io.StringIO()
+    anns_buffer = io.StringIO()
 
-        for label in response['Labels']:
+    for label in response['Labels']:
 
-            if label['Name'] == target_class:
+        if label['Name'] == target_class:
 
-                for instance_nmr in range(len(label['Instances'])):
-                    bound_box = label['Instances'][instance_nmr]['BoundingBox']
-                    x = bound_box['Left']
-                    y = bound_box['Top']
-                    w = bound_box['Width']
-                    h = bound_box['Height']
+            for instance_nmr in range(len(label['Instances'])):
+                bound_box = label['Instances'][instance_nmr]['BoundingBox']
+                x = bound_box['Left']
+                y = bound_box['Top']
+                w = bound_box['Width']
+                h = bound_box['Height']
 
-                    # write detections (anns)
-                    anns_buffer.write(f"0 {(x + w / 2)} {(y + h / 2)} {w} {h}\n")
+                # write detections (anns)
+                anns_buffer.write(f"0 {(x + w / 2)} {(y + h / 2)} {w} {h}\n")
 
-                    # convert to pixel coordinates
-                    x1 = int(x * W)
-                    y1 = int(y * H)
-                    x2 = int((x + w) * W)
-                    y2 = int((y + h) * H)
+                # convert to pixel coordinates
+                x1 = int(x * W)
+                y1 = int(y * H)
+                x2 = int((x + w) * W)
+                y2 = int((y + h) * H)
 
-                    # bounding boxes
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                # bounding boxes
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-        # upload original frames
-        _, raw_buffer = cv2.imencode('.jpg', raw_frame)
-        raw_stream = io.BytesIO(raw_buffer.tobytes())
-        s3.upload_fileobj(raw_stream, bucket_name, f"imgs/frame_{frame_nmr:06d}.jpg")
+    # upload original frames
+    _, raw_buffer = cv2.imencode('.jpg', raw_frame)
+    raw_stream = io.BytesIO(raw_buffer.tobytes())
+    s3.upload_fileobj(raw_stream, bucket_name, f"imgs/frame_{frame_nmr:06d}.jpg")
 
-        # upload processed frames
-        _, processed_buffer = cv2.imencode('.jpg', frame)
-        processed_stream = io.BytesIO(processed_buffer.tobytes())
-        s3.upload_fileobj(processed_stream, bucket_name, f"boxes/frame_{frame_nmr:06d}.jpg")
+    # upload processed frames
+    _, processed_buffer = cv2.imencode('.jpg', frame)
+    processed_stream = io.BytesIO(processed_buffer.tobytes())
+    s3.upload_fileobj(processed_stream, bucket_name, f"boxes/frame_{frame_nmr:06d}.jpg")
 
-        # upload annotations
-        anns_stream = io.BytesIO(anns_buffer.getvalue().encode('utf-8'))
-        s3.upload_fileobj(anns_stream, bucket_name, f"anns/frame_{frame_nmr:06d}.txt")
+    # upload annotations
+    anns_stream = io.BytesIO(anns_buffer.getvalue().encode('utf-8'))
+    s3.upload_fileobj(anns_stream, bucket_name, f"anns/frame_{frame_nmr:06d}.txt")
